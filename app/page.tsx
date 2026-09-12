4801fae82768b688d0128637f81b13a9d0b7a270
yuina368 <y.azarea.w@yahoo.ne.jp>
2026-09-12 20:38:10 +0900

    feat: update page.tsx to todo app

--- app/page.tsx
+++ app/page.tsx
@@ -124,15 +124,15 @@ export default function Home() {
                   削除
                 </button>
                 <button className="text-xs text-blue-500 hover:text-blue-700 font-medium px-2 py-1 rounded transition-colors">
-                  <>
+                  <button className="text-xs text-blue-500 hover:text-blue-700 font-medium px-2 py-1 rounded transition-colors">
                   高
-                  </>
-                  <>
+                  </butto>
+                  <button className="text-xs text-blue-500 hover:text-blue-700 font-medium px-2 py-1 rounded transition-colors">
                   中
-                  </>
-                  <>
+                  </bu>
+                  <button className="text-xs text-blue-500 hover:text-blue-700 font-medium px-2 py-1 rounded transition-colors">
                   低
-                  </>
+                  </button>
                 </button>
                 <button className="text-xs text-yellow-500 hover:text-yellow-700 font-medium px-2 py-1 rounded transition-colors">
                 </button>
